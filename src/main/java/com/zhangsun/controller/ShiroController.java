1b5647c740b143264412c0777b9837e514939b3b
zhangsun <tz483835201@163.com>
2018-12-13 15:19:52 +0800

    shiro Test

---
+++ src/main/java/com/zhangsun/controller/ShiroController.java
@@ -0,0 +1,25 @@
+package com.zhangsun.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ShiroController {
+	
+	@RequestMapping("/login")
+	public String login(HttpServletRequest req, Model model) {
+		String username=req.getParameter("username");
+		String password=req.getParameter("password");
+		System.out.println(username+":"+password);
+		boolean flag=false;
+		if(username.equals("zhangsun") & password.equals("123456")) {
+			flag=true;
+		}
+		System.out.println(flag);
+		model.addAttribute("username", "hello :" + username);
+		return "success";
+	}
+}
